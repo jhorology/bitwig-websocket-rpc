a249fa3b27b11afc452bc6b23e52c47c3799531d
jhorology <jhorology2014@gmail.com>
2019-12-21 02:21:34 +0900

    update deos & test code for ssdp

--- junks/test-ssdp.js
+++ junks/test-ssdp.js
@@ -1,19 +1,51 @@
-var Client = require('node-ssdp').Client,
-    client = new Client();
- 
-client.on('response', (headers, statusCode, rinfo) => {
-  console.log('## response '.padEnd(80, '='))
-  console.log('headers:', headers, 'statusCode:', statusCode, 'rinfo:', rinfo)
-})
- 
-client.search('ssdp:all');
+const {Server, Client} = require('node-ssdp'),
+      server = new Server(),
+      client = new Client()
+      
 
+
+server.addUSN('upnp:rootdevice')
+server.addUSN('urn:schemas-upnp-org:device:MediaServer:1')
+server.addUSN('urn:schemas-upnp-org:service:ContentDirectory:1')
+server.addUSN('urn:schemas-upnp-org:service:ConnectionManager:1')
+
+server.on('advertise-alive', heads => {
+  if (heads.USN === 'uuid:11797f46-6987-4795-ba94-0a6a0c4e8ac5') {
+    console.log('## ssdp:alive '.padEnd(80, '='))
+    console.log('heads:', heads)
+  }
+})
+
+server.on('advertise-bye', heads => {
+  if (heads.USN === 'uuid:11797f46-6987-4795-ba94-0a6a0c4e8ac5') {
+    console.log('## ssdp:byebye '.padEnd(80, '='))
+    console.log('heads:', heads)
+  }
+})
+
+client.on('response', (heads, statusCode, rinfo) => {
+  if (heads.USN === 'uuid:11797f46-6987-4795-ba94-0a6a0c4e8ac5') {
+    console.log('## response '.padEnd(80, '='))
+    console.log('heads:', heads, 'statusCode:', statusCode, 'rinfo:', rinfo)
+  }
+})
+// start server on all interfaces
+server.start()
+  .catch(e => {
+    console.log('Failed to start server:', e)
+  })
+  .then(() => {
+    console.log('Server started.')
+  })
+
+process.on('exit', function() {
+  server.stop() // advertise shutting down and stop listening
+})
+
+client.search('ssdp:all')
 
 setInterval(() => {
   // search for WebSocketRpcServer API10
   console.log('## searching:', 'urn:bitwig-websocket-rpc:service:JSONRPC20:0.2.0-SNAPSHOT')
-  client.search('urn:bitwig-websocket-rpc:service:JSONRPC20:0.2.0-SNAPSHOT');
+  client.search('urn:bitwig-websocket-rpc:service:JSONRPC20:0.2.0-SNAPSHOT')
 }, 5000)
- 
-
-
